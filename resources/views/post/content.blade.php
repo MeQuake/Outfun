15032abb7a7b861b5d34dfbbf71b02703e3df5cf
Adam Mańkowski <maniekmanfred69@gmail.com>
2016-07-24 04:03:56 +0200

    Views split

---
+++ resources/views/post/content.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <div class="panel panel-default panel-post">
+        <div class="panel-heading panel-post clearfix">
+            <a href="{{ url('/post/' .$post->id) }} "><b>{{ $post->title}}</b></a> <small>@nick</small>
+            <div class="pull-right">
+                ocena / data dodania
+            </div>
+        </div>
+        <div class="panel-body">
+            <div class="col-sm-offset-1 col-sm-10">
+                <img src="/images/{{ $post->hash }}.jpg" class="img-responsive" alt="{{ $post->title }}">
+            </div>
+        </div>
+        <div class="panel-footer panel-footer-post">
+            <div class="btn-group btn-group-justified btn-footer-post" role="group" aria-label="Justified button group">
+                <a href="#" class="btn btn-default btn-lg btn-footer-post" role="button"><span class="glyphicon glyphicon-thumbs-up green"></span> Fajne</a>
+                <a href="#" class="btn btn-default btn-lg btn-footer-post" role="button"><span class="glyphicon glyphicon-heart-empty red"></span> Ulubione</a>
+                <a href="#" class="btn btn-default btn-lg btn-footer-post" role="button"><span class="glyphicon glyphicon-comment blue"></span> Skomentuj</a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/sidebars/favourite.blade.php
@@ -0,0 +1,10 @@
+<div class="panel panel-default panel-post">
+    <div class="panel-heading">
+        <b>Reklama / Polecane</b>
+    </div>
+    <div class="panel-body">
+        <div class="col-sm-offset-1 col-sm-10">
+            <img src="/images/nope.jpg" class="img-responsive" alt="Cinque Terre">
+        </div>
+    </div>
+</div>
